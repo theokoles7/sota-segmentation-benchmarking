f228766bf39c096c8de57c82d7a9cfebeead83e1
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-02-11 16:36:41 -0600

    Add main application driver

---
+++ main.py
@@ -0,0 +1,18 @@
+"""Drive applicaiton."""
+
+from utils  import ARGS, BANNER, LOGGER
+
+if __name__ == "__main__":
+    """Execute application command(s)."""
+    
+    try:# Log banner
+        LOGGER.info(BANNER)
+        
+    # Gracefully handle keyboard interruptions
+    except KeyboardInterrupt:   LOGGER.info("Keyboard interruption detected. Aborting operations.")
+        
+    # Catch wildcard errors
+    except Exception as e:      LOGGER.error(f"Unexpected error: {e}")
+    
+    # Exit gracefully
+    finally:                    LOGGER.info("Exiting...")
