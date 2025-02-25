3ba3330e86894f69fd3695afb8320f2a102f40d5
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-02-25 14:48:25 -0600

    Implement scaffolding for model classes

---
+++ models/cnn_based/__init__.py
@@ -0,0 +1,6 @@
+"""CNN-based segmentation models package."""
+
+__all__ = ["DeepLabV3", "UNet"]
+
+from models.cnn_based.deeplab_v3    import DeepLabV3
+from models.cnn_based.unet          import UNet
---
+++ models/foundational/medsam.py
@@ -0,0 +1,26 @@
+"""MedSAM model implementation.
+
+Based on the 2023 paper by Jun Ma et al.:
+https://www.nature.com/articles/s41467-024-44824-z.pdf
+
+Informed by sources:
+* bowang-lab @ GitHub: https://github.com/bowang-lab/MedSAM
+"""
+
+__all__ = ["MedSAM"]
+
+from logging    import Logger
+
+from utils      import LOGGER
+
+class MedSAM():
+    """MedSAM (Foundational) segmentation model."""
+    
+    def __init__(self):
+        """Initialize MedSAM model."""
+        
+        # Initialize logger
+        __logger__: Logger =    LOGGER.getChild("med-sam")
+        
+        # This model has not been implemented
+        raise NotImplementedError(f"MedSAM model is not yet supported.")
