1e299b5a9a64e0fc8a24468ae21a493e5b966e43
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-03-24 14:19:49 -0500

    Implement decoder

--- models/cnn_based/unet/decoding.py
+++ models/cnn_based/unet/decoding.py
@@ -1,10 +1,12 @@
 """Decoder component for U-Net model."""
 
-from logging    import Logger
+from logging                                import Logger
 
-from torch.nn   import Module
+from torch                                  import Tensor
+from torch.nn                               import Module, ModuleList, Upsample
 
-from utilities  import LOGGER
+from models.cnn_based.unet.decoding_block   import DecoderBlock
+from utilities                              import LOGGER
 
 class Decoder(Module):
     """# Decoder component for U-Net model."""
@@ -12,9 +14,9 @@ class Decoder(Module):
     def __init__(self,
         in_channels_skip_connection:    int,
         dimensions:                     int,
-        insampling_type:                str,
+        upsampling_type:                str,
         num_decoding_blocks:            int,
-        normalization:                  str,
+        normalization:                  str =   None,
         preactivation:                  bool =  False,
         residual:                       bool =  False,
         padding:                        int =   0,
@@ -60,6 +62,61 @@ class Decoder(Module):
         super(Decoder, self).__init__()
         
         # Initialize logger.
-        self.__logger__:    Logger =    LOGGER.getChild("unet.decoder")
+        self.__logger__:        Logger =        LOGGER.getChild("unet.decoder")
         
+        # Define attributes.
+        self._decoding_blocks:  ModuleList =    ModuleList()
+        self.dilation:          int =           initial_dilation
         
+        # For each decoding block prescribed...
+        for block in range (num_decoding_blocks):
+            
+            # Append new decoding block.
+            self._decoding_blocks.append(
+                DecoderBlock(
+                    id =                            block,
+                    in_channels_skip_connection =   in_channels_skip_connection,
+                    dimensions =                    dimensions,
+                    upsampling_type =               upsampling_type,
+                    normalization =                 normalization,
+                    preactivation =                 preactivation,
+                    residual =                      residual,
+                    padding =                       padding,
+                    padding_mode =                  padding_mode,
+                    activation =                    activation,
+                    dilation =                      self.dilation,
+                    dropout =                       dropout
+                )
+            )
+            
+            # Update input channels for blocks.
+            in_channels_skip_connection //= 2
+            
+            # Update dilation.
+            if self.dilation is not None: self.dilation //= 2
+            
+    def forward(self,
+        skip_connections:   list[Tensor],
+        X:                  Tensor
+    ) -> Tensor:
+        """# Forward pass through U-Net decoder.
+
+        ## Args:
+            * skip_connections  (list[Tensor]): List of features maps from the encoder's skip 
+                                                connections, ordered from shallowest to deepest. 
+                                                These will be used in reverse order during 
+                                                decoding.
+            * X                 (Tensor):       Input tensor.
+
+        ## Returns:
+            * Tensor:   Decoded feature map with the same spatial dimensions as the shallowest 
+                        skip connection, ready for final classification.
+        """
+        # For each connection skip/block...
+        for skip_connection, decoding_block in zip(reversed(skip_connections), self.decoding_blocks):
+            
+            # Record output from each block.
+            x = decoding_block(skip_connection, x)
+            
+        # Return final output.
+        return x
