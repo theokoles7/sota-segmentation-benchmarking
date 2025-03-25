39be88c576dbec0b3c4c430fe7276f41f65bfab2
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2025-03-25 15:48:22 -0500

    Add package imports

--- models/cnn_based/__init__.py
+++ models/cnn_based/__init__.py
@@ -3,4 +3,4 @@
 __all__ = ["DeepLabV3", "UNet"]
 
 from models.cnn_based.deeplab_v3    import DeepLabV3
-from models.cnn_based.unet.unet     import UNet
+from models.cnn_based.unet          import UNet
---
+++ models/cnn_based/unet/__init__.py
@@ -0,0 +1,5 @@
+"""U-Net model package."""
+
+__all__ = ["UNet"]
+
+from models.cnn_based.unet.unet import UNet
